e4581190e8a25671fac14b653a9649ac59bb7947
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2021-08-13 10:41:48 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15a5c76b555a3bf7223ecefe322fe137'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64740e7b-ba54-48d3-a51c-9cc33ee5df33'
+};
