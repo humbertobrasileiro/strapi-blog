b3943fccafe7c43d462e58cf2308f5f8a8e7e6ef
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2021-09-04 09:04:46 -0300

    ajuste secret

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '15a5c76b555a3bf7223ecefe322fe137'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
